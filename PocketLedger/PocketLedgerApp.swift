b57117a81a9b6d9b3b0c5b294987ff39294f89a0
Jiaming Guo <64711081+j233guo@users.noreply.github.com>
2025-02-28 10:10:42 +0800

    Initial Commit

---
+++ PocketLedger/PocketLedgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketLedgerApp.swift
+//  PocketLedger
+//
+//  Created by Jiaming Guo on 2025-02-28.
+//
+
+import SwiftUI
+
+@main
+struct PocketLedgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PocketLedgerTests/PocketLedgerTests.swift
@@ -0,0 +1,17 @@
+//
+//  PocketLedgerTests.swift
+//  PocketLedgerTests
+//
+//  Created by Jiaming Guo on 2025-02-28.
+//
+
+import Testing
+@testable import PocketLedger
+
+struct PocketLedgerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
