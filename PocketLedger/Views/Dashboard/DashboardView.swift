223fd09efeb7ae13b1ed75515da903dad27350d4
Jiaming Guo <64711081+j233guo@users.noreply.github.com>
2025-03-10 11:47:42 +0800

    added contents in dashboard view

--- PocketLedger/Views/Dashboard/DashboardView.swift
+++ PocketLedger/Views/Dashboard/DashboardView.swift
@@ -7,13 +7,41 @@
 
 import SwiftUI
 
+enum NavigationRoute {
+    case setting
+}
+
 struct DashboardView: View {
+    @State private var showSettingsView: Bool = false
+    @State private var showAddTransactionView: Bool = false
+    
     var body: some View {
-        NavigationStack {
+        NavigationStack{
             List {
-                
+                Button {
+                    showAddTransactionView = true
+                } label: {
+                    HStack {
+                        Spacer()
+                        Text("Log a New Transaction")
+                        Spacer()
+                    }
+                }
             }
             .navigationTitle("Dashboard")
+            .toolbar {
+                ToolbarItem {
+                    Button("Settings", systemImage: "gearshape") {
+                        showSettingsView = true
+                    }
+                }
+            }
+            .sheet(isPresented: $showAddTransactionView) {
+                AddTransactionView()
+            }
+            .sheet(isPresented: $showSettingsView) {
+                SettingsView()
+            }
         }
     }
 }
