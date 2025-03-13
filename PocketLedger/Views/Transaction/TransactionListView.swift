9348c34429af8dc9416e30e1e42f0baba2f12e53
Jiaming Guo <64711081+j233guo@users.noreply.github.com>
2025-03-13 14:18:55 +0800

    supported filtering transactions with date

--- PocketLedger/Views/Transaction/TransactionListView.swift
+++ PocketLedger/Views/Transaction/TransactionListView.swift
@@ -27,40 +27,6 @@ struct TransactionListRowView: View {
     }
 }
 
-#Preview {
-    let expenseCategory = TransactionCategory(
-        name: "Dining",
-        transactionType: .expense,
-        isCustom: false,
-        index: 1,
-        icon: "fork.knife"
-    )
-    let incomeCategory = TransactionCategory(
-        name: "Payroll",
-        transactionType: .income,
-        isCustom: false,
-        index: 0,
-        icon: "dollarsign.circle"
-    )
-    let expenseTransaction = Transaction(
-        transactionType: .expense,
-        amount: 20.0,
-        date: .now,
-        category: expenseCategory,
-        paymentType: .cash
-    )
-    let incomeTransaction = Transaction(
-        transactionType: .income,
-        amount: 100.0,
-        date: .now,
-        category: incomeCategory
-    )
-    List {
-        TransactionListRowView(transaction: expenseTransaction)
-        TransactionListRowView(transaction: incomeTransaction)
-    }
-}
-
 struct GroupedTransactionListView: View {
     var transactions: [Transaction]
     
@@ -92,10 +58,75 @@ struct GroupedTransactionListView: View {
     }
 }
 
+struct TransactionListFilterView: View {
+    @Binding var expanded: Bool
+    @Binding var startDate: Date
+    @Binding var endDate: Date
+    
+    var body: some View {
+        VStack {
+            Button {
+                withAnimation {
+                    expanded.toggle()
+                }
+            } label: {
+                HStack {
+                    Spacer()
+                    Label("Filter", systemImage: "line.3.horizontal.decrease")
+                        .bold()
+                    Image(systemName: expanded ? "chevron.down.circle" : "chevron.right.circle")
+                }
+                .padding(.horizontal)
+                .frame(maxWidth: .infinity)
+            }
+            .buttonStyle(.borderless)
+            
+            if expanded {
+                VStack {
+                    HStack {
+                        Text("Date Range")
+                            .fontWeight(.medium)
+                            .foregroundStyle(.secondary)
+                        DateFilterView(startDate: $startDate, endDate: $endDate)
+                    }
+                }
+            }
+        }
+        .padding(.horizontal)
+        .padding(.bottom, 10)
+        .background(Color(.systemGroupedBackground))
+    }
+}
+
 struct TransactionListView: View {
     @State private var showAddTransactionView: Bool = false
+    @State private var filterExpanded: Bool = false
+    @State private var startDate = Calendar.current.startOfDay(for: .now.addingTimeInterval(-30*24*3600))
+    @State private var endDate = Calendar.current.startOfDay(for: .now)
+    @State private var transactionQueryPredicate: Predicate<Transaction>?
     
-    @Query private var transactions: [Transaction]
+    @Query(sort: \Transaction.date, order: .reverse) private var transactions: [Transaction]
+    
+    private var transactionPredicate: Predicate<Transaction> {
+        let adjustedEndDate = Calendar.current.date(byAdding: .day, value: 1, to: endDate)!
+        return #Predicate {
+            $0.date >= startDate && $0.date < adjustedEndDate
+        }
+    }
+    
+    private var filteredTransactions: [Transaction] {
+        if !filterExpanded {
+            return transactions
+        } else {
+            do {
+                return try transactions.filter { transaction in
+                    try transactionPredicate.evaluate(transaction)
+                }
+            } catch {
+                return []
+            }
+        }
+    }
     
     var body: some View {
         NavigationStack {
@@ -108,8 +139,13 @@ struct TransactionListView: View {
                             .font(.caption)
                     }
                     .foregroundStyle(.secondary)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
                 } else {
-                    GroupedTransactionListView(transactions: transactions)
+                    VStack(spacing: 0) {
+                        TransactionListFilterView(expanded: $filterExpanded, startDate: $startDate, endDate: $endDate)
+                        Divider()
+                        GroupedTransactionListView(transactions: filteredTransactions)
+                    }
                 }
             }
             .navigationTitle("Transactions")
@@ -123,15 +159,45 @@ struct TransactionListView: View {
             .sheet(isPresented: $showAddTransactionView) {
                 AddTransactionView()
             }
+            .animation(.smooth, value: filteredTransactions)
         }
     }
 }
 
 #Preview {
+    let expenseCategory = TransactionCategory(
+        name: "Dining",
+        transactionType: .expense,
+        isCustom: false,
+        index: 1,
+        icon: "fork.knife"
+    )
+    let incomeCategory = TransactionCategory(
+        name: "Payroll",
+        transactionType: .income,
+        isCustom: false,
+        index: 0,
+        icon: "dollarsign.circle"
+    )
+    let expenseTransaction = Transaction(
+        transactionType: .expense,
+        amount: 20.0,
+        date: .now,
+        category: expenseCategory,
+        paymentType: .cash
+    )
+    let incomeTransaction = Transaction(
+        transactionType: .income,
+        amount: 100.0,
+        date: .now,
+        category: incomeCategory
+    )
     if let container = createPreviewModelContainer() {
-        TransactionListView()
+        container.mainContext.insert(expenseTransaction)
+        container.mainContext.insert(incomeTransaction)
+        return TransactionListView()
             .modelContainer(container)
     } else {
-        TransactionListView()
+        return TransactionListView()
     }
 }
